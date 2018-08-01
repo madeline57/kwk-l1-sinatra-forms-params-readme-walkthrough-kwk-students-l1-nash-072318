d407303116b006baba0cd1703a625721f4564d92
madeline57 <maddiejobeth@aol.com>
2018-08-01 15:29:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,7 +5,9 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
-
-  # Add your post route and action below
+post '/food' do 
+  @name = params[:name]
+  @favorite_food = params[:favorite_food]
+end
 
 end
